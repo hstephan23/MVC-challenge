90b0303ed4b5aa4bc17f8f89a8124f57eb26bdb6
hstephan23 <harrison@stephanclan.com>
2024-02-06 20:14:46 -0700

    added the finishing touches on the main format, just need to do handlebars and finish up the other pieces

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -9,10 +9,30 @@ router.post('/', withAuth, async (req, res) => {
             user_id: req.session.user_id
         });
 
-        res.status(200).json(newProject);
+        res.status(200).json(newPost);
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Blogs.destroy({
+            where: {
+                id: req.params/id,
+                user_id: req.session.user_id
+            },
+        });
+
+        if (!postData) {
+            res.status(4040).json({ message: 'No post found with this id!'});
+            return;
+        }
+        
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
